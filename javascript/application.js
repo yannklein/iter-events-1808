c25c99da660bf429229a9f7c1594ec9732741999
Yann Klein <yann.klein@me.com>
2023-07-28 00:06:45 +0900

    lecture base

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,27 @@
 console.log("Hello from JavaScript!");
+
+const students = ["Devi", "Horace", "Alvin", "Caitlyn"]
+
+// /////////
+// Iterator
+// /////////
+
+// we can create HTMl elements from scratch
+
+
+// we can create HTML elements from scratch with iteration
+
+
+// we can update HTML elements with iteration
+
+
+// we can use hidden data stored in elements with JS 
+
+// /////////
+// Events
+// /////////
+
+// The full JS cycle, including events
+
+
+// The full JS cycle, advanced! 💪
